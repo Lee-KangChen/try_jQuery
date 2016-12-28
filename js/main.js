f2460ff3eadb5e817ec7b612d7baf4a89bd5a041
Lee-KangChen <Lee-KangChen@1-167-87-236.dynamic.hinet.net>
2016-12-28 20:23:37 +0800

    show

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ function main(){
   $('.skillset').hide();
   $('.skillset').fadeIn(1000);
   $('.projects').hide();
+
   $('.projects-button').on('click', function() {
+    $('.projects').show();
   });
 }
 $(document).ready(main);
